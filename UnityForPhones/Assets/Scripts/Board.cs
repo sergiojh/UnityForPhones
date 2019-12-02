67a92c743867c7fb2771a4195863575cdc96adef
Pablo Gómez Calvo <pabgom03@ucm.es>
2019-12-02 11:42:45 +0100

    Prototipo de parseo de JSON

---
+++ UnityForPhones/Assets/Scripts/Board.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Path
+{
+    public int x { get; set; }
+    public int y { get; set; }
+}
+
+public class Board
+{
+    public int index { get; set; }
+    public List<string> layout { get; set; }
+    public List<Path> path { get; set; }
+}
---
+++ UnityForPhones/Assets/Scripts/JSONMapReader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using Newtonsoft.Json;
+public class JSONMapReader: MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string json = File.ReadAllText(Application.dataPath + "/Resources/Maps/map1.json");
+        var model = JsonConvert.DeserializeObject<Board>(json);
+        Debug.Log(model.path[0].x);
+        Debug.Log(model.path[0].y);
+        Debug.Log(model.path[1].x);
+        Debug.Log(model.path[1].y);
+        Debug.Log(model.path[2].x);
+        Debug.Log(model.path[2].y);
+
+    }
+}
